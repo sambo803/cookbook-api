b08abb3008287fe7b5194ab6b4ed6f82667c3648
samuel <sbacon803@gmail.com>
2022-07-27 08:56:10 -0600

    add recipes

---
+++ db/migrate/20220727141418_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :title
+      t.string :chef
+      t.string :ingredients
+      t.string :directions
+      t.integer :prep_time
+
+      t.timestamps
+    end
+  end
+end
